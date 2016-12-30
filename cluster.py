f9a93cfe029591b068f0a43eb04d0d06531ffb2b
goytia54 <goytia54@users.noreply.github.com>
2016-12-30 13:17:18 -0700

    Update cluster.py

--- cluster.py
+++ cluster.py
@@ -41,12 +41,14 @@ def near_neigh(box_dim,all_pos):
     nn = [[0 for y_t in range(num_part)]for x_t in range(num_part)]
     in_cluster = [-1 for x_t in range(num_part)]
     clusterSize = [0 for x_t in range(num_part)]
-    cluster_cl = [[0 for y_t in range(num_part)] for x_t in range(num_part)]    
+    cluster_cl = [[0 for y_t in range(num_part)] for x_t in range(num_part)]
+
+    #cycle through all molecules to build a neighbor list within the cut off radius 
     for i in range(0,len(all_pos)): # (n(n+1))/2
         for j in range(i+1,len(all_pos)): #len(g1_array) repalced with 10
             xDiff = abs(float(all_pos[i][1])-float(all_pos[j][1]))
             yDiff = abs(float(all_pos[i][2])-float(all_pos[j][2]))
-            xDiff = abs(xDiff - lx * int(round(xDiff/lx)))
+            xDiff = abs(xDiff - lx * int(round(xDiff/lx))) #account for mirror image 
             yDiff = abs(yDiff - ly * int(round(yDiff/ly)))
             if xDiff**2 + yDiff**2 <= rc**2:
                 nn[i][n_index[i]] = j 
@@ -61,8 +63,11 @@ def cluster_analyze(num_part,all_pos,n_index):
     a_count,b_count,ic,nic,same_nn,likeneigh = 0.0,0.0,0.0,0.0,0.0,0.0
     atomsInCluster = 0.0
     ic_index = []
+    
+    # Cycle through all molecules to start building the cluster lists
     for i in range(0,num_part):
         find_cluster(i,1,n_index)
+    # determine the type of neighbor for each molecule and if in cluster, 3 being one handedness and 4 bein the other
     for j in range(0,nr_clusters):
     	if clusterSize[j] >= 3:
 	   ic_index.append(j)
@@ -78,11 +83,11 @@ def cluster_analyze(num_part,all_pos,n_index):
 	    	nic +=1 #count of atoms not in cluster
         a_count = 0
         b_count = 0
+    #calculating the neighbor density of each cluster
     for l in range(0,len(ic_index)):
 	atomsInCluster += clusterSize[ic_index[l]]
     	for m in range(0,clusterSize[ic_index[l]]):
 	   atom_id = cluster_cl[ic_index[l]][m]
-	   'atom of cluster {0}'.format(l),atom_id
 	   for n in range(0,n_index[atom_id]):
 		if int(all_pos[atom_id][0]) == int(all_pos[nn[atom_id][n]][0]):
 		   same_nn +=1
