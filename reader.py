0a14b339b2f90ccea3f5ffe6b06b6e6ef3390b36
goytia54 <goytia54@users.noreply.github.com>
2016-12-30 13:59:00 -0700

    Update reader.py

--- reader.py
+++ reader.py
@@ -1,11 +1,17 @@
 import settings
 
+'''##############################################################################
+reads the output file from the simulation to extract all positions, number of atoms
+and types for use in cluster
+############################################################################'''
+
 def readTraj(m_ID):
 	infile = settings.rs_d_file
 	pos = []
 	type_list = []
 	num_atoms = 4*m_ID
 	with open(infile,'r') as f:
+		# skipping the headers
         	for line in f:
 			if '<position num="{0}">'.format(str(num_atoms)) in line.strip():
 				break
@@ -26,6 +32,7 @@ def readTraj(m_ID):
 	all_pos = [[0 for y_t in xrange(4)] for x_t in xrange(len(pos)/4)]
 	x = 0
 	k=0
+	#correlating types with positions
 	for j in xrange(len(pos)):
 		if type_list[j] == '4' or type_list[j] == '5':
 			all_pos[k][0] = type_list[j]
